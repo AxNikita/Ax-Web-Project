c4321764b1662be23008807ed9c93da1dc61c80c
AxNikita <51452797+AxNikita@users.noreply.github.com>
2020-08-22 08:03:25 +0600

    + Инициализировал проект

---
+++ src/main/java/com/axproject/axwebproject/AxWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.axproject.axwebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxWebProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axproject/axwebproject/AxWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axproject.axwebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
